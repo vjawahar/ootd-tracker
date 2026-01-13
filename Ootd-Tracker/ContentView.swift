8f507645082d7b058c96f7bece48de0d65c40248
vjawahar <varshaj06@gmail.com>
2026-01-12 19:56:37 -0800

    Initial Commit

---
+++ Ootd-Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ootd-Tracker
+//
+//  Created by Varsha on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ootd-Tracker/Ootd_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ootd_TrackerApp.swift
+//  Ootd-Tracker
+//
+//  Created by Varsha on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Ootd_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
